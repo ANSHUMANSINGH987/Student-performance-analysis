f4182cc2ad841122480660c2df1bea12c1e3039e
ANSHUMANSINGH987 <anshumansingh987265@gmail.com>
2026-07-06 22:44:35 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,35 +47,42 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
 
-            params = {
-                "Random Forest": {
-                    "n_estimators": [50, 100],
-                    "max_depth": [None, 10],
-                },
+            params={
                 "Decision Tree": {
-                    "criterion": ["squared_error", "friedman_mse"],
-                    "max_depth": [None, 5, 10],
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
                 },
-                "Gradient Boosting": {
-                    "learning_rate": [0.05, 0.1],
-                    "n_estimators": [50, 100],
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-                "Linear Regression": {
-                    "fit_intercept": [True, False],
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-                "XGBRegressor": {
-                    "learning_rate": [0.05, 0.1],
-                    "n_estimators": [50, 100],
-                    "max_depth": [3, 5],
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
                 },
-                "CatBoosting Regressor": {
-                    "depth": [4, 6],
-                    "learning_rate": [0.05, 0.1],
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
                 },
-                "AdaBoost Regressor": {
-                    "learning_rate": [0.05, 0.1],
-                    "n_estimators": [50, 100],
-                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
             }
        
 
